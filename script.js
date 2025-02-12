b21b8f2c792965a66a7caf4ac57723fec0f910e3
Victor-Avilla <victorfonsecaavila@gmail.com>
2025-02-11 22:33:23 -0300

    algoritm

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-const inputTask= document.querySelector('input')
+const inputTask = document.querySelector('input')
+const taskList = document.querySelector('ol')
 newTask = () => {
-    alert(inputTask.value)
+
+    taskList.innerHTML += 
+       ` <ol class="list">
+            <li>
+                <label for="check" class="task">${inputTask.value}</label>
+            </li>
+            
+            <input type="checkbox" id="check">
+        </ol>`
+
+    // alert(inputTask.value)
 }
